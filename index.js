4ccc0381ab486a71d4d95ddb4118c785f89bbe81
Josh Larson <jplhomer@gmail.com>
2018-09-04 19:25:53 -0500

    Add some fun stuff

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ class Foo
 
     console.log("how are you?")
   }
+
+  goForIt () {
+    console.log('you can do it, man!')
+
+    console.log(`I promise it is worth it`);
+
+    this.fire()
+  }
+
+  fire () {
+    console.log('BOOM!')
+  }
 }
