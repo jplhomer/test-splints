2b9385a3454b3b64b13fa7f8129b2fc7def2b5e5
Josh Larson <jplhomer@gmail.com>
2018-09-04 19:11:04 -0500

    Add some things

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ class Foo
   constructor()  {
     console.log('hello, world');
 
+    this.callMeMaybe();
+
     console.log("how are you?")
   }
 
   fireMe() {
     console.log('JK I hope i don\'t get fired');
+  }
+
+  callMeMaybe () {
+    // Hey, I just met you
+    // And this is crazy
   }
 }
